ea6c8f5f0e0bd0487744bf62ebd7ae859fc5e333
Chris Waigl <chris.waigl@gmail.com>
2023-03-16 11:25:17 -0800

    more workflow code

--- dsworkflow/rda_JRA55_yr.py
+++ dsworkflow/rda_JRA55_yr.py
@@ -16,12 +16,14 @@ import http.cookiejar
 from dotenv import load_dotenv
 from multiprocessing import Pool
 
+# https://rda.ucar.edu/data/ds628.0/anl_land125/1958/anl_land125.065_snwe.1958010100_1958123118
+
 NUMPROC = 20
 CHUNK = 16 * 1024
 OUTPATH_test = Path("../../working/")
-OUTPATH = Path("/center1/DYNDOWN/cwaigl/ERA5_WRF/era5_grib/")
+OUTPATH = Path("/center1/DYNDOWN/cwaigl/ERA5_WRF/jra55_grib/")
 LOGINURL = "https://rda.ucar.edu/cgi-bin/login"
-PRODUCTURL = "http://rda.ucar.edu/data/ds633.0/"
+PRODUCTURL = "http://rda.ucar.edu/data/ds628.0/"
 VERBOSE = True
 OVERWRITE = False
 
@@ -30,57 +32,35 @@ load_dotenv()
 rdauser = os.getenv("RDAUSER")
 rdapass = os.getenv("RDAPASS")
 
-year = 2022
-month = 11
-folder = "e5.oper.an.pl"
-varsets_folders = {
-    "e5.oper.an.pl" : {
-        "ll025sc": [
-            '128_129_z', '128_130_t',  '128_157_r', 
-        ],
-        "ll025uv": [
-            '128_131_u', '128_132_v',
-        ],
-    },
-    "e5.oper.an.sfc" : {
-        "ll025sc": [
-            '128_031_ci', '128_032_asn', '128_033_rsn', '128_034_sstk', '128_039_swvl1',
-            '128_040_swvl2', '128_041_swvl3', '128_042_swvl4', '128_134_sp', '128_139_stl1',
-            '128_141_sd', '128_151_msl', '128_165_10u', '128_166_10v', '128_167_2t', 
-            '128_168_2d', '128_170_stl2', '128_183_stl3', '128_235_skt', '128_236_stl4' ,
-        ],
-    },
-}
+startyear = 1958
+endyear = 2022
+folder = "anl_land"
+var = "065_snwe"
 listoffiles = []
-# listoffiles=["e5.oper.an.sfc/202112/e5.oper.an.sfc.128_141_sd.ll025sc.2021120100_2021123123.grb",
-            #  "e5.oper.an.sfc/202112/e5.oper.an.sfc.128_151_msl.ll025sc.2021120100_2021123123.grb"]
 
-def get_localpth(mthstr, firsthr, lasthr, folder, varclass, varname):
-    return f"{folder}/{mthstr}/{folder}.{varname}.{varclass}.{firsthr}_{lasthr}.grb"
+# https://rda.ucar.edu/data/ds628.0/anl_land/2014/anl_land.065_snwe.reg_tl319.2014020100_2014022818
+
+def get_localpth(firsthr, lasthr, folder, varname):
+    yr = firsthr[:4]
+    return f"{folder}/{yr}/{folder}.{varname}.reg_tl319.{firsthr}_{lasthr}"
 
 def get_monthstr(yr, mth):
     return f"{str(yr)}{str(mth).zfill(2)}"
 
-def get_filelist(yr, mth):
+def get_filelist(startyear, endyear):
     filelist = []
-    mthstr = get_monthstr(yr, mth)
-    num_days = cal.monthrange(yr, mth)[1]
-    days = [dt.date(yr, mth, day) for day in range(1,num_days+1)]
-    for folder in varsets_folders:
-        for varclass in varsets_folders[folder]:
-            if folder == "e5.oper.an.pl":
-                for day in days:
-                    firsthr = day.strftime("%Y%m%d00")
-                    lasthr = day.strftime("%Y%m%d23")
-                    for var in varsets_folders[folder][varclass]:
-                        fnpth = get_localpth(mthstr, firsthr, lasthr, folder, varclass, var)
-                        filelist.append(fnpth)
-            elif folder == "e5.oper.an.sfc":
+    for yr in range(startyear, endyear+1):
+        if yr < 2014:
+            firsthr = f"{yr}010100"
+            lasthr = f"{yr}123118"
+            filelist.append(get_localpth(firsthr, lasthr, folder, var))
+        else:
+            for mth in range(1, 13):
+                num_days = cal.monthrange(yr, mth)[1]
+                days = [dt.date(yr, mth, day) for day in range(1,num_days+1)]
                 firsthr = days[0].strftime("%Y%m%d00")
-                lasthr = days[-1].strftime("%Y%m%d23")
-                for var in varsets_folders[folder][varclass]:
-                    fnpth = get_localpth(mthstr, firsthr, lasthr, folder, varclass, var)
-                    filelist.append(fnpth)
+                lasthr = days[-1].strftime("%Y%m%d18")
+                filelist.append(get_localpth(firsthr, lasthr, folder, var))
     return filelist
 
 
@@ -113,8 +93,7 @@ def get_urlopener(rdauser, rdapass):
     return opener
 
 def process_file(fileID):
-    mthstr = get_monthstr(year, month)
-    outpath = OUTPATH / mthstr
+    outpath = OUTPATH 
     outpath.mkdir(parents=True, exist_ok=True)
     opener = get_urlopener(rdauser, rdapass)
     idx = fileID.rfind("/")
@@ -145,7 +124,7 @@ if __name__ == "__main__":
 
     start_time = time.perf_counter()
     if not listoffiles:
-        listoffiles = get_filelist(year, month)
+        listoffiles = get_filelist(startyear, endyear)
     print(f"Downloading {len(listoffiles)} files.")
     # print(listoffiles)
 
