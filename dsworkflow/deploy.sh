bf3bf38404dcb86d9f71bed23ee7c217b3d3c3f4
Chris Waigl <chris.waigl@gmail.com>
2023-03-22 19:06:22 -0800

    screwing around with deploy script to get key-based ssh to work :/

--- dsworkflow/deploy.sh
+++ dsworkflow/deploy.sh
@@ -2,4 +2,4 @@
 
 deploychinook="/center1/DYNDOWN/cwaigl/ERA5_WRF/scripts"
 
-rsync -avz --progress * cwaigl@chinook.alaska.edu:${deploychinook}
+rsync -avz --progress -e 'ssh -i ~/.ssh/id_rsa.pub' * cwaigl@chinook.alaska.edu:${deploychinook}
