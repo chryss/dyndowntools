ea6196f14c9a15f0179939094b99842d29be4058
Chris Waigl <chris.waigl@gmail.com>
2023-03-24 14:11:17 -0800

    pre and postprocessing

--- dsworkflow/preprocess_era.sh
+++ dsworkflow/preprocess_era.sh
@@ -4,33 +4,36 @@
 source /home/cwaigl/.bashrc
 module purge
 module load data/CDO/1.7.2-pic-intel-2016b
-conda activate cwtools
+conda activate dyndown
 umask 002
 
 # constants
 BASEDIR=/center1/DYNDOWN/cwaigl/ERA5_WRF
-MONTHDIR="202212"
+value=${1:-the default value}
+MONTHDIR=${1:-"202212"}
+SCRIPTDIR=`pwd`
 
-# VARIDS=("39" "40" "41" "42")
-# SOILMOISTURELABELS=("swvl1" "swvl2" "swvl3" "swvl4")
-# SOILMOISTUREPREFIX="e5.oper.an.sfc.128_0"
-# SOILMOISTURESUFFIX=".ll025sc.2022120100_2022123123.grb"
+# preprocess snow
+python preprocess_snow.py ${MONTHDIR}
 
 cd ${BASEDIR}/era5_grib/${MONTHDIR}
 mkdir -p archive
 
 # Take care of the soil moisture files 
-cdo -expr,'var39 = ((var39 > 0.0)) ? var39 : (0.01)'\
+cdo -expr,'var39 = ((var39 > 0.1)) ? var39 : (0.01)'\
  e5.oper.an.sfc.128_039_swvl1.ll025sc.${MONTHDIR}0100_${MONTHDIR}??23.grb\
  pos_e5.oper.an.sfc.128_039_swvl1.ll025sc.${MONTHDIR}.grb
-cdo -expr,'var40 = ((var40 > 0.0)) ? var40 : (0.01)'\
+cdo -expr,'var40 = ((var40 > 0.1)) ? var40 : (0.01)'\
  e5.oper.an.sfc.128_040_swvl2.ll025sc.${MONTHDIR}0100_${MONTHDIR}??23.grb\
  pos_e5.oper.an.sfc.128_040_swvl2.ll025sc.${MONTHDIR}.grb
-cdo -expr,'var41 = ((var41 > 0.0)) ? var41 : (0.01)'\
+cdo -expr,'var41 = ((var41 > 0.1)) ? var41 : (0.01)'\
  e5.oper.an.sfc.128_041_swvl3.ll025sc.${MONTHDIR}0100_${MONTHDIR}??23.grb\
  pos_e5.oper.an.sfc.128_041_swvl3.ll025sc.${MONTHDIR}.grb
-cdo -expr,'var42 = ((var42 > 0.0)) ? var42 : (0.01)'\
+cdo -expr,'var42 = ((var42 > 0.1)) ? var42 : (0.01)'\
  e5.oper.an.sfc.128_042_swvl4.ll025sc.${MONTHDIR}0100_${MONTHDIR}??23.grb\
  pos_e5.oper.an.sfc.128_042_swvl4.ll025sc.${MONTHDIR}.grb
+
+# move changed files to archive
 mv e5.oper.an.sfc.128_*swvl?.ll025sc.*.grb archive/
+mv e5.oper.an.sfc.128_141* archive/
 
