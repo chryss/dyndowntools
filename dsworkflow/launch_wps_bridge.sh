63675684e813a08f71f7ddc5e0aa1778623e05aa
Chris Waigl <chris.waigl@gmail.com>
2023-03-30 20:40:17 -0800

    workflow

--- dsworkflow/launch_wps_bridge.sh
+++ dsworkflow/launch_wps_bridge.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# echo an error message before exiting and stop on error
+set -e
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 # environment 
 BASEDIR=/center1/DYNDOWN/cwaigl/ERA5_WRF
 MONTHLABEL=$1
@@ -8,30 +12,40 @@ umask 002
 
 source /home/cwaigl/.bashrc
 conda activate dyndown
+module purge
+module load data/netCDF-Fortran/4.4.4-pic-intel-2016b
 
 # First of all generate a link directory for month and previous month
 PREVMONTH=$(date -d "${MONTHLABEL}01 - 1 month" +%Y%m)
 LINKDIR=${BASEDIR}/era5_grib/${MONTHLABEL}_B
-mkdir $LINKDIR
-ln -s ${BASEDIR}/era5_grib/${MONTHLABEL}/e5.oper.an.pl*${MONTHLABEL}01*.grb ${LINKDIR}
-ln -s ${BASEDIR}/era5_grib/${MONTHLABEL}/e5.oper.an.pl*${MONTHLABEL}02*.grb ${LINKDIR}
-ln -s ${BASEDIR}/era5_grib/${MONTHLABEL}/e5.oper.an.pl*${MONTHLABEL}03*.grb ${LINKDIR}
-ln -s ${BASEDIR}/era5_grib/${MONTHLABEL}/*e5.oper.an.sfc*.grb ${LINKDIR}
-ln -s ${BASEDIR}/era5_grib/${PREVMONTH}/e5.oper.an.pl*${PREVMONTH}3*.grb ${LINKDIR}
-ln -s ${BASEDIR}/era5_grib/${PREVMONTH}/e5.oper.an.pl*${PREVMONTH}29*.grb ${LINKDIR}
-ln -s ${BASEDIR}/era5_grib/${PREVMONTH}/e5.oper.an.pl*${PREVMONTH}28*.grb ${LINKDIR}
-ln -s ${BASEDIR}/era5_grib/${PREVMONTH}/*e5.oper.an.sfc*.grb ${LINKDIR}
-ln -s ${BASEDIR}/era5_grib/invar/*.grb ${LINKDIR}
+printf '%s %s\n' "$(date)" "Making links in ${LINKDIR}"
+mkdir -p $LINKDIR
+
+if [[ ! ( -d "$LINKDIR" && $(ls -lAF $LINKDIR | wc -l) -lt 85) ]]; then
+    cd $LINKDIR
+    ln -s ${BASEDIR}/era5_grib/${MONTHLABEL}/e5.oper.an.pl*${MONTHLABEL}01*.grb .
+    ln -s ${BASEDIR}/era5_grib/${MONTHLABEL}/e5.oper.an.pl*${MONTHLABEL}02*.grb .
+    ln -s ${BASEDIR}/era5_grib/${MONTHLABEL}/*e5.oper.an.sfc*.grb .
+    ln -s ${BASEDIR}/era5_grib/${PREVMONTH}/e5.oper.an.pl*${PREVMONTH}3*.grb .
+    ln -s ${BASEDIR}/era5_grib/${PREVMONTH}/e5.oper.an.pl*${PREVMONTH}29*.grb .
+    ln -s ${BASEDIR}/era5_grib/${PREVMONTH}/e5.oper.an.pl*${PREVMONTH}28*.grb .
+    ln -s ${BASEDIR}/era5_grib/${PREVMONTH}/*e5.oper.an.sfc*.grb .
+    ln -s ${BASEDIR}/era5_grib/invar/*.grb .
+    cd $SCRIPTDIR
+else 
+    printf '%s %s\n' "$(date)" "${LINKDIR} already contains necessary links to GRIB files"
+fi
 
 # clone the WPS prototype directory
 WPSDIR=${BASEDIR}/WPS${MONTHLABEL}_B
+printf '%s %s\n' "$(date)" "Cloning WPS directory to ${WPSDIR}"
 cp -r ${BASEDIR}/WPS_dyndown_archivedir ${WPSDIR}
 
 python generate_namelists.py -t wps ${MONTHLABEL}_B
 
 cd ${WPSDIR}
 ./link_grib.csh ${LINKDIR}/*.grb
-./ungrib.exe
-./metgrid.exe
+./ungrib.exe > /dev/null
+./metgrid.exe > /dev/null
 
 rm FILE*
--- dsworkflow/launch_wps_month.sh
+++ dsworkflow/launch_wps_month.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# echo an error message before exiting and stop on error
+set -e
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 # environment 
 BASEDIR=/center1/DYNDOWN/cwaigl/ERA5_WRF
 MONTHLABEL=$1
 SCRIPTDIR=`pwd`
 umask 002
 
+module purge
+module load data/netCDF-Fortran/4.4.4-pic-intel-2016b
 source /home/cwaigl/.bashrc
 conda activate dyndown
 
@@ -16,8 +22,8 @@ cp -r ${BASEDIR}/WPS_dyndown_archivedir ${WPSDIR}
 python generate_namelists.py -t wps ${MONTHLABEL}
 
 cd ${WPSDIR}
-./link_grib.csh ${BASEDIR}/era5_grib/${MONTHLABEL}/*.grb
-./ungrib.exe
-./metgrid.exe
+./link_grib.csh ${LINKDIR}/*.grb
+./ungrib.exe > /dev/null
+./metgrid.exe > /dev/null
 
 rm FILE*
