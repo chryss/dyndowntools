a689cbe8f9789c7cf915ff3dca444cc72fd7d7a4
Chris Waigl <cwaigl@alaska.edu>
2023-06-13 23:59:56 -0800

    making deploy script machine specific

---
+++ dsworkflow/deploy.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+import os
+import subprocess
+
+deploychinook="/center1/DYNDOWN/cwaigl/ERA5_WRF/scripts/"
+hostname = os.uname().nodename
+
+if hostname in ['discernment.local']: 
+    subprocess.call(['rsync', '-avzu', '--progress', '-e', 'ssh -i ~/.ssh/id_rsa.pub', '--include=*', '.', f'cwaigl@chinook.alaska.edu:{deploychinook}'])
+    # rsync -avzu --progress -e 'ssh -i ~/.ssh/id_rsa.pub' cwaigl@chinook.alaska.edu:${deploychinook}/* . 
+elif hostname.startswith('chinook'):
+    subprocess.call(['rsync', '-avzu', '--progress', '--include=*', '.', deploychinook])
--- dsworkflow/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-deploychinook="/center1/DYNDOWN/cwaigl/ERA5_WRF/scripts"
-
-rsync -avzu --progress -e 'ssh -i ~/.ssh/id_rsa.pub' * cwaigl@chinook.alaska.edu:${deploychinook}
-rsync -avzu --progress -e 'ssh -i ~/.ssh/id_rsa.pub' cwaigl@chinook.alaska.edu:${deploychinook}/* . 
