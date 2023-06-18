87ef79f5153505ffc7be27a0ffa1954ebf0e54f2
Chris Waigl <chris.waigl@gmail.com>
2023-06-17 18:09:16 -0800

    working on status logging

--- dsworkflow/adhoc_download.sh
+++ dsworkflow/adhoc_download.sh
@@ -3,17 +3,17 @@
 # environment 
 BASEDIR=/center1/DYNDOWN/cwaigl/ERA5_WRF
 SCRIPTDIR=$(pwd)
-LOGFILE=${$BASEDIR}/
+# LOGFILE=${$BASEDIR}/
 umask 002
 
-# source /home/cwaigl/.bashrc
-# conda activate dyndown
+source /home/cwaigl/.bashrc
+conda activate dyndown
 
 # constants
 # YEARS=(2021 2020)
-YEARS=(2022)
-MONTHS=( 09 08 07 )
-# MONTHS=(12 11 10 09 08 07 06 05 04 03 02 01)
+YEARS=(2019)
+# MONTHS=( 09 08 07 )
+MONTHS=(12 11 10 09 08 07 06 05 04 03 02 01)
 if [[ $# -gt 0 ]]
     then YEARS=($1)
 fi
@@ -22,7 +22,7 @@ for year in "${YEARS[@]}" ; do
     for month in "${MONTHS[@]}" ; do
         printf '%s %s\n' "$(date)" "Downloading ERA5 for ${year}${month}"
         python rda_month.py ${year}${month} > /dev/null
-#        printf '%s %s\n' "$(date)" "Preprocessing ERA5 for ${year}${month}"
-#        bash preprocess_era.sh ${year}${month} & 
+        printf '%s %s\n' "$(date)" "Preprocessing ERA5 for ${year}${month}"
+        bash preprocess_era.sh ${year}${month} & 
     done
 done
