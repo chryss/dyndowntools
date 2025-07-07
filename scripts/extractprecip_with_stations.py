a265d5a3573c51ede02cbea54735bce14a4a575f
Chris Waigl <cwaigl@alaska.edu>
2025-07-06 20:59:24 -0800

    precipitation extraction

--- scripts/extractprecip_with_stations.py
+++ scripts/extractprecip_with_stations.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from functools import partial
-from multiprocessing import get_context
+from joblib import Parallel, delayed
 import time
 import sys
 import datetime as dt
@@ -9,13 +9,6 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import logging
-
-READAPPROACHES = {
-    1: "multiprocessing w/ span",
-    2: "xarray open_mfdataset",
-    3: "loop"
-}
-READAPPROACH = 3
 
 logger = logging.getLogger(__name__)
 logger.setLevel('INFO')
@@ -30,12 +23,12 @@ logger.addHandler(handler)
 NUMBER_OF_CORES = 4
 projdir = Path().resolve().parents[0]
 outdir = projdir / "evaluation/working"
-datadir = Path(f"/import/beegfs/CMIP6/wrf_era5/")
+datadir = Path(f"/import/SNAP/cwaigl/wrf_era5")
 
 # settings for years and location 
 teststation = 'FAIRBANKS INTL AP'
-startyear = 1981
-endyear = 1984
+startyear = 1951
+endyear = 1970
 timezoneoffset = -9  # h for Alaska Standard Time vs UTC 
 var = 'precip'
 outfilepatt = f"{var}_{teststation.replace(' ','_')}_{startyear}_{endyear}"
@@ -64,7 +57,7 @@ def station2df(stationpth):
 
 def getweatherstationdata(stations):
     weatherstationframes = {}
-    for idx, record in stations.iterrows():
+    for _, record in stations.iterrows():
         stationdatapth = weatherstationdir / f"{record['name'].replace(' ', '_')}{weatherstationfnpatt}"
         try:
             weatherstationframes[record['name']] = station2df(stationdatapth)
@@ -88,13 +81,16 @@ def getXY(lat, lon, dataarray):
     ([yloc], [xloc]) = np.where(d == np.min(d))
     return xloc, yloc
 
-def getdates(station, stationDFs, startyr, endyr):
-    dates = stationDFs[station].index.strftime('%Y-%m-%d').to_list()
-    return [item for item in dates if int(item[:4]) in range(startyr, endyr+1)] + [f'{endyr+1}-01-01']
+def getdates(yr):
+    return [dateitem.date() for dateitem in pd.date_range(f'{yr}-01-01', f'{yr}-12-31')] + [dt.date(yr+1, 1, 1)]
 
 def process_file_at_loc(filepath, x=100, y=100):
     with xr.open_dataset(filepath, engine='netcdf4') as src:
-        return (src.rainnc + src.rainc).isel(south_north=y, west_east=x).load().drop_vars(['XLAT', 'XLONG', 'XTIME'])
+        rain = (src.rainnc + src.rainc).isel(
+            south_north=y, west_east=x).load().drop_vars(['XLAT', 'XLONG', 'XTIME'])
+        src.close()
+        del src
+    return rain
 
 def main():
     logger.info(f"Working on precip data for {teststation}")
@@ -112,6 +108,7 @@ def main():
     years = range(startyear, endyear+1)
     for res in resolutions:
         outfn = f"{outfilepatt}_{res}km.csv"
+        (outdir / outfn).unlink(missing_ok=True)
         curr_time_start = time.perf_counter() 
         logger.info(f"getting ERA5 data for {res} km at {teststation}")
         xloc, yloc = None, None
@@ -119,8 +116,8 @@ def main():
         thisdatadir = datadir / f"{str(res).zfill(2)}km/"
         for yr in years: 
             logger.info(f"Working on {yr}")
-            dates = getdates(teststation, weatherstationDFs, yr, yr)
-            filepaths = [thisdatadir / f"{datestr[:4]}/{filepattern}_{datestr}.nc" for datestr in dates]
+            dates = getdates(yr)
+            filepaths = [thisdatadir / f"{dateitem.year}/{filepattern}_{dateitem}.nc" for dateitem in dates]
             logger.debug(f"{filepaths[0]}, {filepaths[-1]}")
             # we first need to find the i, j coordinates of our location
             if (yr == startyear) and filepaths:
@@ -129,21 +126,18 @@ def main():
             logger.debug(f"{test_lat}, {test_lon}, {xloc}, {yloc}")
             # data from downscaled ERA5 
             process_file = partial(process_file_at_loc, x=xloc, y=yloc)
-            with get_context('spawn').Pool(NUMBER_OF_CORES) as pool:
-                parallel_results = pool.map(process_file, filepaths, 3)
-                # pool.close()
-                # pool.join()
-            all_rain = xr.concat(parallel_results, dim='Time').to_dataframe(name=f'precip_mm_ERA5_{res}km')
+            results = Parallel(n_jobs=NUMBER_OF_CORES)(
+                delayed(process_file)(pth) for pth in filepaths)
+            all_rain = xr.concat(results, dim='Time').to_dataframe(name=f'precip_mm_ERA5_{res}km')
             all_rain.index = all_rain.index + dt.timedelta(hours=-9)
             all_rain = all_rain.resample('D').sum()
-
             # put it together
             compDF = all_rain[1:-1].merge(precipDF[f'{yr}-01-01':f'{yr}-12-31'], left_index=True, right_index=True )
             compDF['month'] = list(map(lambda x: calendar.month_abbr[x], compDF.index.month))
 
             logger.info(f"Writing/appending to {outfn}")
             with open(outdir / outfn, 'a', newline='') as dst:
-                compDF.to_csv(dst, float_format='%.3f')
+                compDF.to_csv(dst, float_format='%.3f', header=dst.tell()==0)
 
             curr_time_end = time.perf_counter()
             elapsed_time = curr_time_end - curr_time_start 
