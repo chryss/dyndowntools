9473953f722a32c6c07b626a953a0071b88f73e1
Chris Waigl <cwaigl@alaska.edu>
2025-09-20 18:30:55 -0800

    wind speed gridded analysis

--- scripts/monthavg_wspd_loop.py
+++ scripts/monthavg_wspd_loop.py
@@ -7,7 +7,7 @@ import pandas as pd
 import xarray as xr
 from  dask.distributed import Client
 
-resolution = 4         # 4 or 12 km
+resolution = 12         # 4 or 12 km
 datadir = Path(f"/import/SNAP/cwaigl/wrf_era5/{str(resolution).zfill(2)}km/")
 filepattern = f"era5_wrf_dscale_{resolution}km"
 chunking = {
@@ -15,7 +15,7 @@ chunking = {
     12: ['auto', 'auto']
 }
 outfn = f"windavg_monthly_{resolution}km.nc"
-testyears_1 = ['2010', '2011', ]
+testyears_1 = ['2010', '2011', '2012', '2012']
 years = [str(yr) for yr in range(1981, 2021)]
 months = [str(item).zfill(2) for item in range (1, 13)]
 Nmonths = 12
@@ -24,8 +24,8 @@ if __name__ == '__main__':
     client = Client()
     print(client.dashboard_link)
     initial_start_time = time.perf_counter()
-    loopyr = testyears_1
-    # loopyr = years
+    # loopyr = testyears_1
+    loopyr = years
     loopmth = months[:Nmonths]
     collect = []
     accperiods = 0
@@ -54,10 +54,11 @@ if __name__ == '__main__':
         collect.append(monthly_acc)
         end_time = time.perf_counter()
         print(f"Loop for {yr} before saving file: {end_time - start_time:.2f}", "seconds")
-        # write out file for each 10 completed years 
-        # if idx%10 == 9:
+        # write out file for each N completed years 
+        # if idx%3 == 1:
         #     monthly_acc.to_netcdf(
-        #         "snowacc.nc", 
+        #         "windacc.nc",
+        #         mode='a', 
         #         encoding={'acsnow': {"zlib": True, "complevel": 5}})
         #     end_time = time.perf_counter ()
         #     print(f"Loop for {yr} after saving intermediate result to file: {end_time - start_time:.2f}", "seconds")
@@ -69,7 +70,7 @@ if __name__ == '__main__':
     print("Saving file")
     total_acc.to_netcdf(
         outfn, 
-        encoding={'wspd10': {"zlib": True, "complevel": 3}})
+        encoding={'wspd10': {"zlib": True, "complevel": 5}})
     end_time = time.perf_counter()
     print("Execution time:")
     print(f"{end_time - initial_start_time:.2f}", "seconds")
